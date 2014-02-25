cbe62026fa4a2b119bff47bc66644b883d6219ea
Sebastian Blumenthal <blumenthal@locomotec.com>
2014-02-25 17:06:51 +0100

    Bugfix for error in confuguration of trigger in usc file.

--- start_demo.sh
+++ start_demo.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 source ./env.sh
-cd $MICROBLX_DIR && luajit $BRICS_3D_FUNCTION_BLOCKS_DIR/lua/function_blocks_test.lua 
+#cd $MICROBLX_DIR && luajit $BRICS_3D_FUNCTION_BLOCKS_DIR/lua/function_blocks_test.lua
+cd $MICROBLX_DIR && tools/ubx_launch -c $BRICS_3D_FUNCTION_BLOCKS_DIR/lua/function_blocks_test.usc -webif
 #cd $BRICS_3D_FUNCTION_BLOCKS_DIR # go back
